e769fbf496c61f1533ef0b96d7e32010bbae6631
Mathew-Caldwell <20143330@tafe.wa.edu.au>
2026-08-06 11:56:36 +0800

    code comments

--- MathewCaldwellLibaryApp/Libary.cs
+++ MathewCaldwellLibaryApp/Libary.cs
@@ -18,6 +18,13 @@ namespace MathewCaldwellLibaryApp
             this.address = address;
         }
 
+        /// <summary>
+        /// Add a customer and all their detail to libary customer list
+        /// </summary>
+        /// <param name="customerName"></param>
+        /// <param name="customerAddress"></param>
+        /// <param name="email"></param>
+        /// <param name="phoneNumber"></param>
         public void AddCustomer(string customerName, string customerAddress, string email, int phoneNumber)
         {
             int ID = masterCustomerList.Count + 1;
@@ -26,6 +33,13 @@ namespace MathewCaldwellLibaryApp
             masterCustomerList.Add(customer);
         }
 
+        /// <summary>
+        /// Add a new book and all its details to libary book list
+        /// </summary>
+        /// <param name="bookName"></param>
+        /// <param name="author"></param>
+        /// <param name="yearPublished"></param>
+        /// <param name="dueDate"></param>
         public void AddBook(string bookName, string author, int yearPublished, DateOnly dueDate)
         {
             int ID = masterBooKList.Count + 1;
@@ -34,6 +48,11 @@ namespace MathewCaldwellLibaryApp
             masterBooKList.Add(book);
         }
 
+        /// <summary>
+        /// Changes books status to checkedOut and records which customer has the book
+        /// </summary>
+        /// <param name="bookID"></param>
+        /// <param name="customerName"></param>
         public void CheckOutBook(int bookID, string customerName)
         {
             Book book = masterBooKList[0];
@@ -48,6 +67,10 @@ namespace MathewCaldwellLibaryApp
             book.customerWithBook = customerName;
         }
 
+        /// <summary>
+        /// Sets book status to available and reverts who has possesion back to the libary
+        /// </summary>
+        /// <param name="bookID"></param>
         public void ReturnBook(int bookID)
         {
             Book book = masterBooKList[0];
@@ -59,9 +82,14 @@ namespace MathewCaldwellLibaryApp
                 }
             }
             book.status = Status.available;
-            book.customerWithBook = "Libary";
+            book.customerWithBook = this.name;
         }
 
+        /// <summary>
+        /// Allows customer to reserve a book
+        /// </summary>
+        /// <param name="bookID"></param>
+        /// <param name="customerName"></param>
         public void ReserveBook(int bookID, string customerName)
         {
             Book book = masterBooKList[0];
@@ -76,6 +104,10 @@ namespace MathewCaldwellLibaryApp
             book.customerWithBook = customerName;
         }
 
+        /// <summary>
+        /// Allows customer to report a book lost 
+        /// </summary>
+        /// <param name="bookID"></param>
         public void ReportBookLost(int bookID)
         {
             Book book = masterBooKList[0];
