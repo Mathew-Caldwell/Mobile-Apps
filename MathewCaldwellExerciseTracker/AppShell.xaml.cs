6ec8783a216048dec6100357b893a4097be68f5f
Mathew-Caldwell <20143330@tafe.wa.edu.au>
2026-08-06 12:02:38 +0800

    Created and set up exercise tracker assignment

---
+++ MathewCaldwellExerciseTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MathewCaldwellExerciseTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
